4719621f4f84d6d21b5612dc3682607ebd852cff
SERGEY SAVVOV <sersavvov@yandex.ru>
2019-05-27 12:14:28 +0300

    Update with_procces_and_treads.py

--- 1. Погружение в Python/week 5/Работа с сетью, сокеты/with_procces_and_treads.py
+++ 1. Погружение в Python/week 5/Работа с сетью, сокеты/with_procces_and_treads.py
@@ -1,8 +1,5 @@
 # обработка нескольких соединений одновременно, процессы и потоки
 
-
-# При запуске на ПК выпадает ошибка: Обычно разрешается только одно использование адреса сокета (протокол/сетевой адрес/порт)
-
 import socket
 import multiprocessing
 import threading
@@ -21,22 +18,22 @@ def process_requests(conn, addr):
 # каждый worker запущен в отдельном процессе
 def worker(sock):
     while True:
-        print(f'Pid = {os.getpid()}')
         conn, addr = sock.accept()
+        print(f'Pid = {os.getpid()}')
         th = threading.Thread(target=process_requests, args=(conn, addr))
         th.start()
+if __name__ ==  '__main__':
+    with socket.socket() as sock:
+        sock.bind(("", 10001))
+        sock.listen()
 
-with socket.socket() as sock:
-    sock.bind(("", 10001))
-    sock.listen()
+        # при создании нового процесса, мы полностью копируем socket
+        workers_count = 3
+        workers_list = [multiprocessing.Process(target=worker, args=(sock,))
+                       for _ in range(workers_count)]
 
-    # при создании нового процесса, мы полностью копируем socket
-    workers_count = 3
-    workers_list = [multiprocessing.Process(target=worker, args=(sock,))
-                   for _ in range(workers_count)]
+        for w in workers_list:
+            w.start()
 
-    for w in workers_list:
-        w.start()
-
-    for w in workers_list:
-        w.join()
+        for w in workers_list:
+            w.join()
