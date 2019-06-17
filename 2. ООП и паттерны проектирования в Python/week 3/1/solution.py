a50b70ab2acd441588fa73ee687b091592418886
SERGEY SAVVOV <sersavvov@yandex.ru>
2019-06-17 16:35:49 +0300

    Create solution.py

---
+++ 2. ООП и паттерны проектирования в Python/week 3/1/solution.py
@@ -0,0 +1,99 @@
+from abc import abstractmethod, ABC
+
+
+class AbstractEffect(Hero, ABC):
+    def __init__(self, base):
+        self.base = base
+
+    @abstractmethod
+    def get_positive_effects(self):
+        pass
+
+    @abstractmethod
+    def get_negative_effects(self):
+        pass
+
+    @abstractmethod
+    def get_stats(self):
+        pass
+
+
+class AbstractPositive(AbstractEffect):
+    def get_negative_effects(self):
+        return self.base.get_negative_effects()
+
+
+class AbstractNegative(AbstractEffect):
+    def get_positive_effects(self):
+        return self.base.get_positive_effects()
+
+
+class Berserk(AbstractPositive):
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats["HP"] += 50
+        stats["Strength"] += 7
+        stats["Endurance"] += 7
+        stats["Agility"] += 7
+        stats["Luck"] += 7
+        stats["Perception"] -= 3
+        stats["Charisma"] -= 3
+        stats["Intelligence"] -= 3
+        return stats
+
+    def get_positive_effects(self):
+        return self.base.get_positive_effects() + ["Berserk"]
+
+
+class Blessing(AbstractPositive):
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats["Strength"] += 2
+        stats["Endurance"] += 2
+        stats["Agility"] += 2
+        stats["Luck"] += 2
+        stats["Perception"] += 2
+        stats["Charisma"] += 2
+        stats["Intelligence"] += 2
+        return stats
+
+    def get_positive_effects(self):
+        return self.base.get_positive_effects() + ["Blessing"]
+
+
+class Weakness(AbstractNegative):
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats["Strength"] -= 4
+        stats["Endurance"] -= 4
+        stats["Agility"] -= 4
+        return stats
+
+    def get_negative_effects(self):
+        return self.base.get_negative_effects() + ["Weakness"]
+
+
+class EvilEye(AbstractNegative):
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats["Luck"] -= 10
+        return stats
+
+    def get_negative_effects(self):
+        return self.base.get_negative_effects() + ["EvilEye"]
+
+
+class Curse(AbstractNegative):
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats["Strength"] -= 2
+        stats["Endurance"] -= 2
+        stats["Agility"] -= 2
+        stats["Luck"] -= 2
+        stats["Perception"] -= 2
+        stats["Charisma"] -= 2
+        stats["Intelligence"] -= 2
+        return stats
+
+    def get_negative_effects(self):
+        return self.base.get_negative_effects() + ["Curse"]
